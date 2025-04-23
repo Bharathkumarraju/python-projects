8e3b884a68eadfa7ef53232e8d00f57d38624721
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-04-24 06:21:41 +0800

    kick start python

---
+++ project1/1.hello_world.py
@@ -0,0 +1,25 @@
+print('Hello World!')
+
+print("Python3 is awesome!")
+
+my_favourite_number = 5
+print(f"my favourite number is:",    my_favourite_number)
+
+pi = 3.14
+print(pi)
+
+my_favourite_number = pi
+print("My favourite number is:", my_favourite_number)
+print("")
+print("City:", "Singapore", "is", "the", "best city", "in the world!")
+
+print(range(1,10))
+print(type(range(1,10)))
+
+for i in range(1,10):
+    print(i)
+
+city =  "Singapore"
+mcd_locations = 200
+
+print("the city is:", city, "and the number of MCD locations are:",mcd_locations)

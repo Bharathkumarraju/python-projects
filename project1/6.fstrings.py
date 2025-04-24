e2eaa955b2454045af75063bbcb5443aa799532e
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-04-25 05:44:08 +0800

    fstrings

---
+++ project1/6.fstrings.py
@@ -0,0 +1,7 @@
+number = 5.5
+
+print(f"the number is {number} and its of type {type(number)}")
+
+number_str = str(number)
+
+print(f"the number is {number_str} and its of type {type(number_str)}")

416930fcd12f232fe8cea79f1b4971520e1f15c5
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-04-25 05:13:59 +0800

    Arithmetic operations

---
+++ project1/4.arithmetic.sh
@@ -0,0 +1,8 @@
+Operator	Syntax	Meaning
++	        x + y	Addition
+-	        x - y	Subtraction
+*	        x * y	Multiplication
+/	        x / y	Division
+//	      x // y	Quotient
+%	        x % y	Remainder
+**	      x ** y	Exponentiation

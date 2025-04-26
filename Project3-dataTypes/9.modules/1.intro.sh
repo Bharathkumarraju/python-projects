602272daaf651a4356682737737c5ac6e72871d3
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-04-27 06:08:47 +0800

    python modules'

---
+++ Project3-dataTypes/9.modules/1.intro.sh
@@ -0,0 +1,15 @@
+what is a module?
+
+A module is a file containing a set of functions that we might want to use in our application. Once we import a module, we can use all the functions defined inside that module.
+
+Why modules?
+
+You might have heard that Python is used for data analytics, web development, automation, etc.
+
+To solve these tasks, we take the help of modules rather than writing code from scratch.
+
+For example, if we need to perform tasks related to
+
+data analytics, we will use modules such as NumPy, pandas, etc.
+web development, we will use modules such as Django, Flask, etc.
+csv files, we will use modules such as csv, pandas, etc.

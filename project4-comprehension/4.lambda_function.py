eebae993256e02e040bf04e082a5db0acd946c0e
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-04-27 16:51:49 +0800

    lamda argument: return value

---
+++ project4-comprehension/4.lambda_function.py
@@ -0,0 +1,18 @@
+"""
+Lamnda function is a regualtr function with some differences
+1. lambda function doesn't have a name
+2. the body of a lambda function has only one expression
+
+"""
+
+# Regular function
+
+def double(n):
+    return (n**2)
+print(double(6))
+
+
+# lambda function syntax is --- just argument and return value
+
+suqare= lambda n: n**2
+print(suqare(6))
---
+++ project4-comprehension/5.lambda_function.py
@@ -0,0 +1,6 @@
+square = lambda n: n*n
+
+number = float(input("enter a number: "))
+
+result = square(number)
+print(result)

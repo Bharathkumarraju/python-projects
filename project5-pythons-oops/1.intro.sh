490dd354bf6fb0618e616db43f7081a21e2f79d0
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-04-28 02:37:49 +0800

    In OOPs world variables are called attributes and functions are called methods

---
+++ project5-pythons-oops/1.intro.sh
@@ -0,0 +1,40 @@
+Object-Oriented Programming is a popular technique to solve programming problems by creating objects.
+
+Classes and Objects
+  There are two steps involved in creating objects:
+
+1. Define a class
+2. Create objects from the class
+
+
+Lets say Student class has two variables
+  name and
+  score,
+and a function check_pass_fail().
+
+One thing to remember about classes is that they cannot store data. We can store data after we create objects from the class.
+
+Think of a class as a blueprint of a house.
+It contains all the details about the floors, doors, windows, etc. Based on these descriptions, we build the house.
+The actual physical house is the object.
+
+
+
+
+Creating Objects:
+Once we define a class, we can create as many objects as we want from the class.
+
+And, we store data in objects (not classes).
+
+In the image, we have created objects student1 and student2 from the Student class.
+
+All the objects of this Student class will have the
+  name variable and the
+  score variable,
+and can use the check_pass_fail() function.
+
+
+So when working with classes and objects
+  variables are called attributes.
+  functions are called methods.
+

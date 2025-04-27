fc30bcb8e727f25d7f2949d7e2cf65e18671a19b
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-04-27 17:04:11 +0800

    Function argument positional arguments and keyword arguments

--- project4-comprehension/5.lambda_function.py
+++ project4-comprehension/5.lambda_function.py
@@ -4,3 +4,9 @@ number = float(input("enter a number: "))
 
 result = square(number)
 print(result)
+
+
+product = lambda x, y : x * y
+
+result = product(25, 25)
+print(result)
---
+++ project4-comprehension/6.function_arguments.py
@@ -0,0 +1,13 @@
+def display_info(name,age):
+    print(name)
+    print(age)
+
+display_info('Hundu', 27)
+
+
+def display_info2(name, age):
+    print(f"name is: {name}")
+    print(f"age is: {age}")
+
+
+display_info2(age=38, name='Bharath')

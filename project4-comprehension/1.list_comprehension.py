930984a27e7a211b74717df7ef375d5cc13ed831
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-04-27 16:05:39 +0800

    python list comprehension

---
+++ project4-comprehension/1.list_comprehension.py
@@ -0,0 +1,8 @@
+numbers = []
+for i in range(1, 9):
+    numbers.append(2 ** i)
+print(numbers)
+
+n = [ 3**i  for i in range(1,6) ]
+print(n)
+
---
+++ project4-comprehension/2.list_comprehension_condition.py
@@ -0,0 +1,4 @@
+numbers = [1, 2, 4, 6 , 7, 9, 5, 11, 14, 17, 15, 18, 18]
+
+even_numbers = [ i for i in numbers if i % 2 == 0]
+print(even_numbers)

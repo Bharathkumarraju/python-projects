ec27187bfef15bd9a2405c1d321636c379547d9b
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-04-28 11:46:19 +0800

    Inheritance in python

---
+++ project5-pythons-oops/23.python_inheritance.py
@@ -0,0 +1,15 @@
+class Animal:
+    def eat(self):
+        print("I can eat!")
+
+class Dog(Animal):
+    def bark(self):
+        print("I can bark!")
+
+dog1 = Dog()
+dog1.bark()
+dog1.eat()
+
+
+animal1 = Animal()
+animal1.eat()
